c0fb6c452e9b8fab228d87cbc72ab134424620dc
Aleksander Wójcik <aleksander.w1992@wp.pl>
2017-08-13 14:05:43 +0200

    adding DomSanitizer - small fix to displaying icons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MdIconRegistry} from '@angular/material';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,9 @@ import {MdIconRegistry} from '@angular/material';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private mdIconRegistry: MdIconRegistry) {
-    mdIconRegistry.addSvgIcon('custom-login', 'assets/icons/login-svg.svg');
-    mdIconRegistry.addSvgIcon('custom-logout', 'assets/icons/logout-svg.svg');
+  constructor(private mdIconRegistry: MdIconRegistry, private sanitizer: DomSanitizer) {
+    mdIconRegistry.addSvgIcon('custom-login', this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/login-svg.svg'));
+    mdIconRegistry.addSvgIcon('custom-logout', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/logout-svg.svg'));
   }
 
   ngOnInit() {
